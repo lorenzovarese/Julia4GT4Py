1e236886df3e0bce8b90abd944cbab08ab89cda6
LorenzoVarese <lorenzovarese@users.noreply.github.com>
2024-06-25 13:45:24 +0200

    Change profiling to PProf to check allocations

--- laplacian_profiling.jl
+++ laplacian_profiling.jl
@@ -2,20 +2,27 @@
 include("laplacian.jl")
 using .Laplacian
 using Profile
+using PProf
 using ProfileView
 
 # Define the test dimensions and generate the test field
-nrows, ncols = 10000, 10000  # Large dimensions for significant profiling data
+nrows, ncols = 1000, 1000  # Large dimensions for significant profiling data
 in_field = rand(Float64, nrows, ncols)  # Randomly generate input field
 
 # Function to profile each implementation
 function profile_laplacian(func, in_field)
     println("Profiling ", func)
-    Profile.clear()  # Clear previous profiling data
-    @profile (for i in 1:10  # Run multiple times to gather more comprehensive data
+    Profile.Allocs.clear()
+    Profile.clear()
+    @pprof (for i in 1:10  # Run multiple times to gather more comprehensive data
         func(in_field)
     end)
-    Profile.print()  # Print the profiling results
+    # For macOS
+    run(`open http://localhost:57599`) # MacOS
+
+    # run(`start http://localhost:57599`) # Linux
+    # run(`xdg-open http://localhost:57599`) # Windows
+    sleep(2)
 end
 
 # Profile each implementation
